36ff4e908644f6d372ba58098dc10bae7813acc0
79855 <leb.alex123@gmail.com>
2023-03-10 12:24:53 +0300

    4. Тесты для стороны прямоугольника. [#159460]. Убрал паблики и пробел

--- src/test/java/ru/job4j/condition/SqAreaTest.java
+++ src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 class SqAreaTest {
 
     @Test
-    public void whenP6K2Square2() {
+    void whenP6K2Square2() {
         int expected = 2;
         int p = 6;
         double k = 2;
@@ -15,7 +15,7 @@ class SqAreaTest {
     }
 
     @Test
-    public void whenP8K2Square3() {
+    void whenP8K2Square3() {
         int expected = 3;
         int p = 8;
         double k = 2;
@@ -24,12 +24,11 @@ class SqAreaTest {
     }
 
     @Test
-    public void whenP6K3Square1() {
+    void whenP6K3Square1() {
         int expected = 1;
         int p = 6;
         double k = 3;
         double out = SqArea.square(p, k);
         Assertions.assertEquals(expected, out, 0.6875);
     }
-
 }
