5a1c4329d93ed7ae1268f3256060c8fcfdbb63a1
79855 <leb.alex123@gmail.com>
2023-03-12 17:46:21 +0300

    4.1.2. Глупый бот [#31323]

---
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,21 @@
+package ru.job4j.condition;
+
+public class DummyBot {
+
+    public static String answer(String question) {
+        if ("Hi, Bot.".equals(question)) {
+            return  "Hi, SmartAss.";
+        }
+        if ("Bye.".equals(question)) {
+            return "See you later.";
+        }
+        return "I don't know. Please, ask another question.";
+    }
+
+    public static void main(String[] args) {
+        String rsl = DummyBot.answer("Hi, Bot.");
+        System.out.println(rsl);
+        rsl = DummyBot.answer("Bye.");
+        System.out.println(rsl);
+    }
+}
