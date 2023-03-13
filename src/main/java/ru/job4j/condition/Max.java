11687fc08e084d16439c03dbdbab05a68615577b
79855 <leb.alex123@gmail.com>
2023-03-13 13:04:11 +0300

    4.2. Тернарное сравнение [#189 #400033]. Написал в одну строчку

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -3,9 +3,6 @@ package ru.job4j.condition;
 public class Max {
 
     public static int max(int left, int right) {
-        if (left >= right) {
-            return left;
-        }
-        return left > right ? left : right;
+        return left >= right ? left : right;
     }
 }
