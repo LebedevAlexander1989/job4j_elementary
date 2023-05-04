01a96fe7f5e16c2e17de142ab4ed81056a0d32d4
79855 <leb.alex123@gmail.com>
2023-05-04 17:21:57 +0300

    5.7. Ипотека [#156315].Рефакторинг

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -6,7 +6,7 @@ public class Mortgage {
         int year = 0;
 
         while (amount > 0) {
-            amount = (amount + amount * percent / 100) - salary;
+            amount += amount * percent / 100 - salary;
             year++;
         }
         return year;
