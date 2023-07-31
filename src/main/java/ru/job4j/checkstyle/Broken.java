3582975ee0ff46ab378050c12dfe84436ed915b2
79855 <leb.alex123@gmail.com>
2023-08-01 00:47:37 +0300

    6. Правила Checkstyle [#504981  [#504981]]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,33 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+
+    private static final int SIZE_OF_EMPTY = 10;
+    private static final String NEW_VALUE = "";
+
+    private String surname;
+    private String name;
+
+    public Broken() {
+
+    }
+
+    void echo() {
+
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void methodOne(int a, int b, int c, int d) {
+
+    }
+
+    void methodTwo(int e, int f, int g, int h) {
+
+    }
+}
+
