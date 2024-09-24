8ecf1a67d062ea310b0cc5958f43bea7f2e468e5
79855 <leb.alex123@gmail.com>
2024-09-24 14:44:47 +0300

    GPT. Нейросети спешат на помощь. [#505156]

--- src/test/java/ru/job4j/calculator/FitTest.java
+++ src/test/java/ru/job4j/calculator/FitTest.java
@@ -21,4 +21,36 @@ class FitTest {
         double out = Fit.womanWeight(in);
         assertEquals(expected, out, 0.01);
     }
+
+    @Test
+    public void whenManHeight187ThenWeight100dot05() {
+        short height = 187;
+        double expected = 100.05;
+        double out = Fit.manWeight(height);
+        assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenManHeight160ThenWeight69dot00() {
+        short height = 160;
+        double expected = 69.00;
+        double out = Fit.manWeight(height);
+        assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenWomanHeight177ThenWeight77dot05() {
+        short height = 177;
+        double expected = 77.05;
+        double out = Fit.womanWeight(height);
+        assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenWomanHeight150ThenWeight46dot00() {
+        short height = 150;
+        double expected = 46.00;
+        double out = Fit.womanWeight(height);
+        assertEquals(expected, out, 0.01);
+    }
 }
